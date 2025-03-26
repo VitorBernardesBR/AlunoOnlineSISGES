ddb39911505d3dd5f0eaef67bab99d580577891f
YuriRamalhoSilva <yuriramalhosilva67@gmail.com>
2025-03-26 18:34:08 -0300

    commit de quase tudo

---
+++ MainScreen/script.js
@@ -0,0 +1,17 @@
+const barra_lateral = document.getElementById('barr_lat');
+const menu_button = document.getElementById('Menu');
+
+function toggleMenu() {
+
+    if(barra_lateral.style.display ==='flex'){
+        barra_lateral.style.display = 'none';
+    } else {
+        barra_lateral.style.display = 'flex';
+    }
+
+}
+
+menu_button.addEventListener('click',function(event){
+    event.stopPropagation();
+    toggleMenu();
+});
