9c829b2be3e54f41fa1091ce28cf50ff81bc6e74
YuriRamalhoSilva <yuriramalhosilva67@gmail.com>
2025-04-09 15:22:52 -0300

    script que edita a cor da prioridade de mensagem

--- MainScreen/script.js
+++ MainScreen/script.js
@@ -16,3 +16,22 @@ menu_button.addEventListener('click',function(event){
     toggleMenu();
     
 });
+
+const divPri = document.getElementById('div-prioridade');
+const rangePri =  document.getElementById('range');
+rangePri.setAttribute('value' , 0);
+rangePri.setAttribute('max' , '2');
+
+let prioridade = 0;
+
+rangePri.addEventListener('change' , function(e){
+    prioridade = e.target.value;
+    switchColor(prioridade);
+});
+
+function switchColor(prioridade){
+    divPri.classList.remove('prioridade-1','prioridade-2','prioridade-3');
+    divPri.classList.add('prioridade-'+prioridade);
+
+    
+}
